6cae842e27a9fe9ebc0f96ac6e9e4f8660c754f8
Andrea Filippi <a.filippi21@studenti.unipi.it>
2024-10-10 17:12:01 +0000

    sostituito test_window con global_sum

--- tests/rocksdb_tests/global_sum_test.cpp
+++ tests/rocksdb_tests/global_sum_test.cpp
@@ -119,7 +119,8 @@ int main(int argc, char *argv[])
                                     .withParallelism(op_degree)
                                     .withKeyBy([](const tuple_t &t) -> size_t { return t.key; })
                                     .withTupleSerializerAndDeserializer(tuple_serializer, tuple_deserializer)
-                                    .setFragmentSize(key_buff);
+                                    .withResultSerializerAndDeserializer(result_serializer, result_deserializer)
+                                    .setWindowBufferSizeBytes(sizeof(tuple_t)*10);
 
         if (tb_win) builder = builder.withTBWindows(std::chrono::microseconds(win_len), std::chrono::microseconds(win_slide));
         else builder = builder.withCBWindows(win_len, win_slide);
